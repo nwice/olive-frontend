e5aada5598a2d4501ee32c061848e4396cc93085
Oliver Green <oliver-green@users.noreply.github.com>
2021-04-05 20:21:01 +0800

    olive-usdt logo, avax-usdt add liq url

--- src/utils/getLiquidityUrlPathParts.ts
+++ src/utils/getLiquidityUrlPathParts.ts
@@ -3,8 +3,9 @@
 // In the URL, using the quote token 'BNB' is represented by 'ETH'
 const getLiquidityUrlPathParts = ({ quoteTokenAdresses, quoteTokenSymbol, tokenAddresses }) => {
   const chainId = process.env.REACT_APP_CHAIN_ID
-  const firstPart = quoteTokenSymbol === 'BNB' ? 'ETH' : quoteTokenAdresses[chainId]
-  const secondPart = tokenAddresses[chainId]
+  // eslint-disable-next-line no-nested-ternary
+  const firstPart = quoteTokenSymbol === 'BNB' ? 'ETH' : quoteTokenSymbol === 'AVAX' ? 'AVAX' : quoteTokenAdresses[chainId]
+  const secondPart = tokenAddresses[chainId] === '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7' ? 'AVAX' : tokenAddresses[chainId]
   return `${firstPart}/${secondPart}`
 }
 
